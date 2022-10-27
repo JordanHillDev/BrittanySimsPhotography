83400bef38e36bc0d896ed1b1437558aa857a173
Jordanhill90 <74479504+Jordanhill90@users.noreply.github.com>
2022-10-27 10:32:50 -0400

    added javascript for modal pop up

---
+++ public/js/main.js
@@ -0,0 +1,53 @@
+const body = document.body;
+const mobileNav = body.querySelector("#mobileNav");
+const mobileMenuLink = body.querySelector("#mobileMenuLink");
+const workDropdownListItem = body.querySelector('#workDropdownListItem')
+const workDropdown = body.querySelector('#workDropdown')
+const imageWrappers = galleryWrapper.querySelectorAll('.img_container')
+const modal = body.querySelector('#modal')
+const modalImg = modal.querySelector('img')
+const closeModalButton = body.querySelector('#closeModalButton')
+
+// <==== Mobile Menu Button ====>
+mobileMenuLink.addEventListener("click", function () {
+    mobileNav.style.height === "0px"
+        ? (mobileNav.style.height = "250px")
+        : (mobileNav.style.height = "0px");
+});
+
+// <==== Dropdown List ==== >
+workDropdownListItem.addEventListener('click', function() {
+    workDropdown.classList.toggle('visibleMenu')
+})
+
+// <==== Close Modal Button ==== >
+closeModalButton.addEventListener('click', function() {
+    modal.style.display = 'none'
+})
+
+
+// <========== Image Modal ==========>
+let currImg = ''
+// <==== Click Image for Pop Up ====>
+imageWrappers.forEach(el => el.addEventListener('click', function(){
+    const img = el.querySelector('img')
+    modal.style.display = 'flex'
+    modalImg.src = img.src
+    currImg = img.src
+}))
+
+// <==== Image Modal Last / Next Buttons ====>
+const lastButton = body.querySelector('#lastButton')
+const nextButton = body.querySelector('#nextButton')
+const imgSrcArr = []
+imageWrappers.forEach(el => imgSrcArr.push(el.childNodes[1].src))
+
+nextButton.addEventListener('click', function(){
+    modalImg.src = imgSrcArr[((imgSrcArr.indexOf(currImg) + 1) % imgSrcArr.length + imgSrcArr.length) % imgSrcArr.length]
+    currImg = modalImg.src
+})
+
+lastButton.addEventListener('click', function(){
+    modalImg.src = imgSrcArr[((imgSrcArr.indexOf(currImg) - 1) % imgSrcArr.length + imgSrcArr.length) % imgSrcArr.length]
+    currImg = modalImg.src
+})
