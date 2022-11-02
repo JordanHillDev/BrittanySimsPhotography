bfa07649c295fa9a2b2b3f75dbf745169d440d04
Jordanhill90 <74479504+Jordanhill90@users.noreply.github.com>
2022-11-02 08:45:07 -0400

    minified normalize and reset css files

--- controllers/main.js
+++ controllers/main.js
@@ -3,13 +3,14 @@ const Image = require("../models/Image");
 module.exports = {
     getIndex: async (req, res) => {
         try {
-         
+            console.time()
             const images = await Image.find({ category: 'concert'}).lean();
             const resizedImages = images.map(ea => {
                 ea.urlBeg= ea.image.slice(0, ea.image.indexOf('upload/') + 7)
                 ea.urlEnd = ea.image.slice(ea.image.indexOf('upload/') + 7)
                 return ea;
             })
+            console.timeEnd()
             res.render("index.ejs", {
                 images: resizedImages
             });
